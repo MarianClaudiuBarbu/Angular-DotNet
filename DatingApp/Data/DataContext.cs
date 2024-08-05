2a33f9d16437eaa3af8c6d8bf245e7124f7bba8b
Barbu Marian Claudiu <barbu.iu.marian@student.utcluj.ro>
2024-08-05 18:11:10 +0300

    Re

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
---
+++ DatingApp/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingApp.Models;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+
+}
